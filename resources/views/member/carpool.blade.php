2dac2cda7c7dedb57d7196adbb9b650fe247370d
mukyisadog <ukfai412@gmail.com>
2023-04-26 10:11:46 +0800

    0426 1010 member page css update

--- resources/views/member/carpool.blade.php
+++ resources/views/member/carpool.blade.php
@@ -7,7 +7,6 @@
 <link rel="stylesheet" href="{{asset('css/member-all.css')}}">
 <link rel="stylesheet" href="{{asset('css/member-carpool.css')}}">
 <script type="text/javascript" src="{{asset('js/member-all.js')}}"></script>
-<!-- <script type="text/javascript" src="{{asset('js/car-record.js')}}"></script> -->
 
 @endsection
 
@@ -67,8 +66,8 @@
                                             <div class="memberState">
                                                 <form action="{{route('comfirmjoin')}}" method="post">
                                                     @csrf
-                                                    <button type="submit" name="cpconfirm" value="1" class="operate">✓</button>
-                                                    <button type="submit" name="cpconfirm" value="2" class="operate">✗</button>
+                                                    <button type="submit" name="cpconfirm" value="1" class="operate">加入</button>
+                                                    <button type="submit" name="cpconfirm" value="2" class="operate">拒絕</button>
                                                     <input type="hidden" name="joiner" value="{{$jo->id}}">
                                                     <input type="hidden" name="cpid" value="{{$c->cpid}}">
                                                 </form>
@@ -80,11 +79,11 @@
                                             @endif
                                         @elseif($jo->status == 1)
                                             <div class="memberState">
-                                                <button name="" id="" class="operate">已加入</button>
+                                                <button name="" id="" class="doneBtn">已加入</button>
                                             </div>
                                         @elseif($jo->status == 2)
                                             <div class="memberState">
-                                                <button name="" id="" class="operate">已拒絕</button>
+                                                <button name="" id="" class="doneBtn">已拒絕</button>
                                             </div>
                                         @endif
                                     </div>
@@ -103,7 +102,9 @@
                 @endif
             </div>
 
-            <div id="join">
+<hr />
+
+            <div class="join">
                 <h2>參加中</h2>
                 @if(empty($cp2) == false)
                     @foreach($cp2 as $c)
@@ -136,8 +137,9 @@
                 @endif
             </div>
 
+<hr />
 
-            <div id="join">
+            <div class="join">
                 <h2>確認中</h2>
                 @if(empty($cp3) == false)
                     @foreach($cp3 as $c)
@@ -149,11 +151,13 @@
                                 <div class="groupName">
                                     {{$c->cptitle}}
                                 </div>
-                                <form action="{{route('canceljoin')}}" method="post">
-                                @csrf
-                                <input type="hidden" name="cpid" value="{{$c->cpid}}">
-                                <button name="" id="" class="operate" type="submit">取消</button>
-                                </form>
+                                <div class="joinMember">
+                                    <form action="{{route('canceljoin')}}" method="post">
+                                        @csrf
+                                        <input type="hidden" name="cpid" value="{{$c->cpid}}">
+                                        <button name="" id="" class="operate" type="submit">取消</button>
+                                        </form>        
+                                </div>
                             </div>
                         </a>
                     @endforeach
@@ -164,11 +168,9 @@
                 @endif
             </div>
 
+<hr />
 
-
-
-
-            <div id="finished">
+            <div class="join">
                 <h2>歷史紀錄</h2>
                 @if(empty($cp4) == false)
                     @foreach($cp4 as $c)
