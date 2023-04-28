e91e67886cff640c1ba8f9dcdab88444d853412f
mukyisadog <ukfai412@gmail.com>
2023-04-28 09:28:26 +0800

    0428 0930 add weather

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,14 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\CarpoolController;
-use App\Http\Controllers\MemberController;
-use App\Http\Controllers\SendJoinNoticeMailController;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\FeelController;
-use App\Http\Controllers\ForumController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\CarpoolController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 // 首頁
 Route::get('/',[IndexController::class,'Index'])->name('home');
+Route::get('/weather',[IndexController::class,'getweather']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/notice', [CarpoolController::class, 'cpjoinNotice'])->name('notice');
